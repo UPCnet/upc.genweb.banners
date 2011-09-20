ad481532e97e6698d2d6660da861c94093d696e4
Ariel <ariel.von.barnekow@upcnet.es>
2011-09-20 13:22:48 +0200

    Fixed: Ordenació segons ordre a la carpeta als portlet de banners

--- upc/genweb/banners/portlets/bannersportlet.py
+++ upc/genweb/banners/portlets/bannersportlet.py
@@ -98,6 +98,7 @@ class Renderer(base.Renderer):
             return catalog(portal_type='Banner',
                        review_state=state,
                        path=banner_container[0].getPath(),
+                       sort_on='getObjPositionInParent',
                        sort_limit=limit)[:limit]
         else:
             return []
