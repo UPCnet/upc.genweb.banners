d64600b7bb2a3bc8e31baacae7ff75094a7a7b84
Victor Fernandez de Alba <victor.fernandez@upcnet.es>
2012-10-31 17:42:25 +0100

    updated to last version and updated catalog metadata

--- upc/genweb/banners/content/Banner.py
+++ upc/genweb/banners/content/Banner.py
@@ -1,17 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# File: Banner.py
-#
-# Copyright (c) 2008 by []
-# Generator: ArchGenXML Version 2.0
-#            http://plone.org/products/archgenxml
-#
-# GNU General Public License (GPL)
-#
-
-__author__ = """unknown <unknown>"""
-__docformat__ = 'plaintext'
-
 from AccessControl import ClassSecurityInfo
 from Products.Archetypes.atapi import *
 from Products.ATContentTypes.content import base
@@ -21,9 +8,6 @@ import interfaces
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
 
 from upc.genweb.banners.config import *
-
-##code-section module-header #fill in your manual code here
-##/code-section module-header
 
 schema = Schema((
 
@@ -57,15 +41,8 @@ schema = Schema((
 ),
 )
 
-##code-section after-local-schema #fill in your manual code here
-##/code-section after-local-schema
-
 Banner_schema = BaseSchema.copy() + \
     schema.copy()
-
-
-##code-section after-schema #fill in your manual code here
-##/code-section after-schema
 
 
 class Banner(base.ATCTContent, BrowserDefaultMixin):
@@ -79,13 +56,7 @@ class Banner(base.ATCTContent, BrowserDefaultMixin):
 
     schema = Banner_schema
 
-    ##code-section class-header #fill in your manual code here
-    ##/code-section class-header
-
-    # Methods
+    def obrirEnFinestraNova(self):
+        return self.getObrirennovafinestra()
 
 registerType(Banner, PROJECTNAME)
-# end of class Banner
-
-##code-section module-footer #fill in your manual code here
-##/code-section module-footer
